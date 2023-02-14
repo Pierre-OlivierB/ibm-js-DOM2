545a66e37ff06c6641c4212da1f41d5fcec3e0ec
unknown <baudinopierreolivier@gmail.com>
2023-02-14 13:58:22 +0100

    add catch content equals to content enter in input

--- index.js
+++ index.js
@@ -40,38 +40,42 @@ buttonHello.addEventListener("click", displayTextVisible);
 // TODO: (c) alertTitle();with alert() : display nth() content by title click
 // *(a)
 const titleCards = document.querySelectorAll("h1");
+const lastTextThirdCard = document.querySelector("#third_card>p:last-of-type");
+const secLastTextThirdCard = document.querySelector(
+  "#third_card>p:nth-last-of-type(2)"
+);
+function partOneDisplay() {
+  let contentNext = document.querySelector("h1:first-child+div");
+  let textContentNext = document.querySelectorAll("h1:first-child+div p");
+  //   console.log(contentNext);
+  //   console.log(textContentNext);
+  contentNext.style.display = "block";
+  textContentNext.forEach((e) => {
+    e.style.opacity = "1";
+  });
+}
+function partTwoDisplay() {
+  let contentNext = document.querySelector("h1:last-of-type+div");
+  //   console.log(contentNext);
+  let textContentNext = document.querySelectorAll("h1:last-of-type+div p");
+  //   console.log(contentNext);
+  //   console.log(textContentNext);
+  contentNext.style.display = "block";
+  textContentNext.forEach((e) => {
+    e.style.opacity = "1";
+  });
+}
 // console.log(titleCards);
 titleCards.forEach((e) => {
   //   console.log(e);
   e.addEventListener("click", () => {
     // console.log(e.textContent);
     if (e.textContent == "Partie 1") {
-      let contentNext = document.querySelector("h1:first-child+div");
-      let textContentNext = document.querySelectorAll("h1:first-child+div p");
-      //   console.log(contentNext);
-      //   console.log(textContentNext);
-      contentNext.style.display = "block";
-      textContentNext.forEach((e) => {
-        e.style.opacity = "1";
-      });
+      partOneDisplay();
     }
     if (e.textContent == "Partie 2") {
-      let contentNext = document.querySelector("h1:last-of-type+div");
-      //   console.log(contentNext);
-      let textContentNext = document.querySelectorAll("h1:last-of-type+div p");
-      //   console.log(contentNext);
-      //   console.log(textContentNext);
-      contentNext.style.display = "block";
-      textContentNext.forEach((e) => {
-        e.style.opacity = "1";
-      });
+      partTwoDisplay();
     }
-    const lastTextThirdCard = document.querySelector(
-      "#third_card>p:last-of-type"
-    );
-    const secLastTextThirdCard = document.querySelector(
-      "#third_card>p:nth-last-of-type(2)"
-    );
     // console.log(secLastTextThirdCard);
     lastTextThirdCard.style.opacity = "1";
     secLastTextThirdCard.style.opacity = "1";
@@ -83,10 +87,10 @@ titleCards.forEach((e) => {
 // !---------------------------------------------------
 // *(b)
 const inputHide = document.querySelector("input[value='Masquer']");
-console.log(inputHide);
+// console.log(inputHide);
 function hideAllDivs() {
   const allParagraphes = document.querySelectorAll("p");
-  console.log(allParagraphes);
+  // console.log(allParagraphes);
   for (let i = 0; i < allParagraphes.length - 2; i++) {
     allParagraphes[i].style.opacity = "0";
   }
@@ -95,3 +99,27 @@ inputHide.addEventListener("click", hideAllDivs);
 
 // !------------------------------------------------------
 //* (c)
+// console.log(title);
+const title = document.getElementById("title");
+// console.log(title.value);
+function catchTextPart(ele) {
+  let contentJoin = "";
+  for (let i = 0; i < ele.length; i++) {
+    contentJoin += ele[i].textContent + ". ";
+  }
+  alert(contentJoin);
+}
+function displayInputValue() {
+  // console.log(title.value);
+  let textContentNext;
+  if (titleCards[title.value - 1].textContent == "Partie 1") {
+    textContentNext = document.querySelectorAll("h1:first-child+div p");
+    partOneDisplay();
+  }
+  if (titleCards[title.value - 1].textContent == "Partie 2") {
+    textContentNext = document.querySelectorAll("h1:last-of-type+div p");
+    partTwoDisplay();
+  }
+  catchTextPart(textContentNext);
+}
+title.addEventListener("change", displayInputValue);
