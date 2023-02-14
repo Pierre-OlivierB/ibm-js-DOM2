57113de23cc6a33ae583a27fedd198d3f8ed214f
unknown <baudinopierreolivier@gmail.com>
2023-02-14 12:20:30 +0100

    add for each cross on click

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ window.addEventListener("load", init);
 // !---------------------------------------------------
 // *Second Card
 const buttonHello = document.getElementById("sec_card");
-console.log(buttonHello);
+// console.log(buttonHello);
 var texteHello = "Hello World ! <br/>";
 function addText() {
   buttonHello.innerHTML += texteHello;
@@ -25,11 +25,11 @@ function addText() {
 // buttonHello.addEventListener("click", addText);
 
 var textField = document.querySelector("#sec_card>p");
-console.log(textField);
+// console.log(textField);
 
 function displayTextVisible() {
   textField.style.opacity = "1";
-  console.log(textField.innerText);
+  //   console.log(textField.innerText);
 }
 
 buttonHello.addEventListener("click", displayTextVisible);
@@ -38,3 +38,37 @@ buttonHello.addEventListener("click", displayTextVisible);
 // TODO: (a) showHxContent();use selector; display content Div on click
 // TODO: (b) hideAllDivs(); display none all div content
 // TODO: (c) alertTitle();with alert() : display nth() content by title click
+// *(a)
+const titleCards = document.querySelectorAll("h1");
+// console.log(titleCards);
+titleCards.forEach((e) => {
+  console.log(e);
+  e.addEventListener("click", () => {
+    console.log(e.textContent);
+    if (e.textContent == "Partie 1") {
+      let contentNext = document.querySelector("h1:first-child+div");
+      let textContentNext = document.querySelectorAll("h1:first-child+div p");
+      //   console.log(contentNext);
+      //   console.log(textContentNext);
+      contentNext.style.display = "block";
+      textContentNext.forEach((e) => {
+        e.style.opacity = "1";
+      });
+    }
+    if (e.textContent == "Partie 2") {
+      let contentNext = document.querySelector("h1:last-of-type+div");
+      console.log(contentNext);
+      let textContentNext = document.querySelectorAll("h1:last-of-type+div p");
+      //   console.log(contentNext);
+      //   console.log(textContentNext);
+      contentNext.style.display = "block";
+      textContentNext.forEach((e) => {
+        e.style.opacity = "1";
+      });
+    }
+  });
+  //   e.addEventListener("click", (el) => {
+  //     console.log(el);
+  //   });
+});
+// *(b)
