30059402eeee8d8cb325560580d64bf6dd5388e7
unknown <baudinopierreolivier@gmail.com>
2023-02-14 11:09:58 +0100

    first card finished

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // console.log("test");
+// *First Card
+function init() {
+  let check = document.querySelector("#findChecked");
+  function addElementInList() {
+    var selected = document.querySelectorAll("*:checked");
+    var result = "Les cases sélectionnées sont :<br> ";
+    for (var i = 0; i < selected.length; i++) {
+      result += selected[i].name + " ";
+    }
+    document.querySelector("#resultat").innerHTML = result;
+  }
+  check.addEventListener("click", addElementInList);
+}
+window.addEventListener("load", init);
+// !---------------------------------------------------
+// *Second Card
